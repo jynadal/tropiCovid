eb6a8c2866c94d51e28b2dba4a1004736f2f2d59
jynadal <nadaljeanyves@hotmail.fr>
2020-06-17 22:45:29 +0200

    Ending Covid-19

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -4,12 +4,15 @@ import CardDeck from 'react-bootstrap/CardDeck';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 import Moment from 'react-moment';
+import Form from 'react-bootstrap/Form';
+import Columns from 'react-columns';
 
 import './App.css';
 
 function App() {
   const [latest, setLatest] = useState([]);
   const [results, setResults] = useState([]);
+  const [searchCountries, setSearchCountries] = useState("");
 
   useEffect(() => {
     axios
@@ -32,26 +35,42 @@ function App() {
   const lastUpdated = date.toString();
   Moment.globalLocale = 'fr';
 
-  const countries = results.map(data => {
+  const filterCountries = results.filter(item => {
+    return searchCountries !== "" ? item.country.includes(searchCountries) : item;
+  })
+
+  
+
+  const countries = filterCountries.map((data, i) => {
     return (
-      <Card bg="light"
+      <Card key={i}
+            bg="light"
             text="dark"
             className="text-center"
             style={{margin:"10px"}}
       >
-        <Card.Body>
-          <Card.Title>{data.country}</Card.Title>
-          <Card.Text>Nombre de cas {data.cases}</Card.Text>
-          <Card.Title>Nombre de décès {data.deaths}</Card.Title>
-          <Card.Text>Nombre de guéris {data.recovered}</Card.Text>
-          <Card.Title>{data.todayDeaths} personnes décédés aujourd'hui</Card.Title>
-          <Card.Text>{data.todayRecovered} personnes guéris aujourd'hui</Card.Text>
-          <Card.Title>{data.continent}</Card.Title>
-          <Card.Text>{data.countryInfo.iso2}</Card.Text>
-        </Card.Body>
+        <Card.Img  variant="top" src={data.countryInfo.flag} />
+                            <Card.Body>
+                              <Card.Title>{data.country}</Card.Title>
+                              <Card.Text>Nombre de cas {data.cases}</Card.Text>
+                              <Card.Text>Nombre de décès {data.deaths}</Card.Text>
+                              <Card.Text>Nombre de guéris {data.recovered}</Card.Text>
+                              <Card.Text>{data.todayDeaths} personnes décédés aujourd'hui</Card.Text>
+                              <Card.Text>{data.todayRecovered} personnes guéris aujourd'hui</Card.Text>
+                              <Card.Text>{data.continent}</Card.Text>
+                              <Card.Text>{data.countryInfo.iso2}</Card.Text>
+                            </Card.Body>        
       </Card>
     );
   });
+
+  var queries = [{
+    columns: 2,
+    query: 'min-width: 500px'
+  }, {
+    columns: 3,
+    query: 'min-width: 1000px'
+  }];
 
   return (<div>
     <CardDeck>
@@ -61,7 +80,7 @@ function App() {
         style={{margin:"10px"}}
   >
     <Card.Body>
-      <Card.Title>Personnes Infectés</Card.Title>
+      <Card.Text>Personnes Infectés</Card.Text>
       <Card.Text>{latest.cases}</Card.Text>
     </Card.Body>
     <Card.Footer>
@@ -73,7 +92,7 @@ function App() {
         className="text-center"
         style={{margin:"10px"}}>
     <Card.Body>
-      <Card.Title>Personnes Décedés</Card.Title>
+      <Card.Text>Personnes Décedés</Card.Text>
       <Card.Text>
         {latest.deaths} Décés du au Coronavirus
       </Card.Text>
@@ -87,7 +106,7 @@ function App() {
         className="text-center"
         style={{margin:"10px"}}>
     <Card.Body>
-      <Card.Title>Personnes Guéries </Card.Title>
+      <Card.Text>Personnes Guéries </Card.Text>
       <Card.Text>
        {latest.recovered} Personnes guéries après avoir contracté la Covid-19
       </Card.Text>
@@ -97,7 +116,19 @@ function App() {
     </Card.Footer>
   </Card>
 </CardDeck> 
-{countries} 
+
+<Form>
+  <Form.Group controlId="formGroupSearch">
+    <Form.Label>Recherche</Form.Label>
+    <Form.Control 
+    type="text" 
+    placeholder="Recherche un Pays"
+    onChange={e => setSearchCountries(e.target.value)} 
+  />
+  </Form.Group>
+</Form>
+
+<Columns queries={queries}>{countries}</Columns>
       
   </div>
   );
